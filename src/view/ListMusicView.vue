da648f338670f6674b0ec5315c94615875f5c647
cuikho210 <tonykms210@gmail.com>
2023-04-28 05:24:10 +1400

    Fix conflig

--- src/view/ListMusicView.vue
+++ src/view/ListMusicView.vue
@@ -1,59 +1,155 @@
 <script setup lang="ts">
-import { ref } from "vue"
-import { ListMusic } from '../util/piano'
+import { ref, reactive, watch } from "vue"
+import { useRoute } from "vue-router"
+import { ListMusicAndCollection, CreateCollection, GetCollectionBreadcrumb } from '../util/music'
 import { ConvertMidiToJsonFromFile } from "../util/converter"
-import MusicFile from "../component/piano/MusicFile.vue"
-import PrimaryButton from "../component/button/PrimaryButton.vue"
+import MusicFileEntry from "../component/piano/MusicFileEntry.vue"
 import PrimaryInput from "../component/input/PrimaryInput.vue"
+import IconOnlyButton from "../component/button/IconOnlyButton.vue"
+import PrimaryButton from "../component/button/PrimaryButton.vue"
+import PrimaryDialog from "../component/layout/PrimaryDialog.vue"
 import type { FileEntry } from "@tauri-apps/api/fs"
 
+let route = useRoute()
 let is_loaded = ref(false)
 let list_music = ref<FileEntry[]>([])
+let list_collection = ref<FileEntry[]>([])
+let collection_breadcrumb = ref<FileEntry[]>([])
 let search_string = ref("")
+let current_path: string | undefined
 
-loadData()
+updateCurrentPath()
+loadCollection()
 
-async function loadData() {
-    list_music.value = await ListMusic()
+watch(route, () => {
+    updateCurrentPath()
+    loadCollection()
+}, { immediate: true })
+
+function updateCurrentPath() {
+    if (route.query.path && typeof route.query.path == "string") {
+        current_path = route.query.path
+    } else {
+        current_path = undefined
+    }
+}
+
+async function loadCollection(path?: string) {
+    if (path) current_path = path
+    
+    let result = await ListMusicAndCollection(current_path)
+    list_collection.value = result.collections
+    list_music.value = result.musics
+    
+    collection_breadcrumb.value = await GetCollectionBreadcrumb(current_path)
+    
     is_loaded.value = true
+    search_string.value = ""
 }
 
 async function importFromMIDI() {
-    await ConvertMidiToJsonFromFile()
-    await loadData()
+    await ConvertMidiToJsonFromFile(current_path)
+    await loadCollection()
 }
+
+let createCollection = reactive({
+    is_show: false,
+    collection_name: "",
+
+    Open() {
+        createCollection.is_show = true
+    },
+
+    Create() {
+        CreateCollection(createCollection.collection_name, current_path)
+        loadCollection()
+        createCollection.collection_name = ""
+        createCollection.is_show = false
+    }
+})
 </script>
 
 <template>
-<section class="container-md">
+<section class="container-md" :key="route.fullPath">
     <div class="navbar">
         <PrimaryInput
-            icon="search"
-            type="search"
-            placeholder="Search"
             class="search"
+            type="search"
+            icon="search"
             v-model="search_string"
+            placeholder="Search..."
         />
 
-        <PrimaryButton
-            icon="file_open"
+        <IconOnlyButton
+            icon="create_new_folder"
+            @click="createCollection.Open"
+            title="Create Collection"
+        />
+
+        <IconOnlyButton
+            icon="audio_file"
             @click="importFromMIDI"
-        >Import from MIDI</PrimaryButton>
+            title="Import MIDI file"
+        />
+    </div>
+
+    <div class="breadcrumb">
+        <div>
+            <RouterLink to="/music">Home</RouterLink>
+        </div>
+
+        <div v-for="entry in collection_breadcrumb" :key="entry.path">
+            / <RouterLink :to="'/music?path=' + entry.path">{{ entry.name }}</RouterLink>
+        </div>
     </div>
 
     <div class="list" v-if="is_loaded">
-        <div class="music" v-for="(music, index) in list_music" :key="index">
-            <MusicFile
-                :name="music.name || '_'"
+        <div v-for="collection in list_collection" :key="collection.path">
+            <MusicFileEntry
+                type="collection"
+                :name="collection.name || '_'"
+                :path="collection.path"
+                @on:remove="loadCollection"
+                @on:rename="loadCollection"
+                v-show="collection.name?.toLowerCase().includes(search_string.toLowerCase())"
+            />
+        </div>
+        
+        <div v-for="music in list_music" :key="music.path">
+            <MusicFileEntry
+                type="music"
+                :name="music.name?.slice(0, music.name?.length - 5) || '_'"
                 :path="music.path"
-                @on:remove_file="loadData"
-                v-show="music.name && music.name.toLowerCase().includes(search_string.toLowerCase())"
+                @on:remove="loadCollection"
+                @on:rename="loadCollection"
+                v-show="music.name?.toLowerCase().includes(search_string.toLowerCase())"
             />
         </div>
     </div>
     <p v-else>
         Loading...
     </p>
+
+    <Transition name="fade-in-fast">
+        <PrimaryDialog
+            title="Create Collection"
+            v-model="createCollection.is_show"
+            v-show="createCollection.is_show"
+        >
+            <PrimaryInput
+                icon="tag"
+                type="text"
+                width="100%"
+                placeholder="Collection name"
+                v-model="createCollection.collection_name"
+            />
+
+            <PrimaryButton
+                icon="add"
+                @click="createCollection.Create"
+            >Create</PrimaryButton>
+        </PrimaryDialog>
+    </Transition>
 </section>
 </template>
 
@@ -61,13 +157,21 @@ async function importFromMIDI() {
 @import "../asset/scss/container.scss";
 
 .navbar {
+    margin-bottom: 1rem;
     display: flex;
     align-items: center;
-    margin-bottom: 1rem;
 
     .search {
         width: 100%;
         margin-right: 7px;
+    }
+}
+
+.breadcrumb {
+    margin: 1rem auto;
+
+    div {
+        display: inline;
     }
 }
 
