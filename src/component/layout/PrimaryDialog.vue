ef8b82a3d25a877fc16a9ce2c83d7b9749d709e1
cuikho210 <tonykms210@gmail.com>
2023-05-03 09:20:32 +0700

    Fix scroll in PrimaryDialog component

--- src/component/layout/PrimaryDialog.vue
+++ src/component/layout/PrimaryDialog.vue
@@ -10,10 +10,6 @@ const emit = defineEmits(["update:modelValue"])
 function hide() {
     emit("update:modelValue", false)
 }
-
-function show() {
-    emit("update:modelValue", true)
-}
 </script>
 
 <template>
@@ -61,18 +57,24 @@ section {
         display: block;
         background-color: var(--color-bg-primary);
         width: 400px;
-        max-width: 100%;
-        max-height: 100%;
+        max-width: calc(100% - 1rem);
+        max-height: calc(100% - 1rem);
         z-index: 41;
         border-radius: 7px;
-        overflow: hidden;
+        overflow-x: hidden;
+        overflow-y: scroll;
         box-shadow: 4px 4px 1rem rgba(0, 0, 0, 0.04);
 
         .header {
+            position: sticky;
+            top: 0px;
+            left: 0px;
             display: flex;
             justify-content: space-between;
             align-items: center;
             border-bottom: 1px solid rgba(var(--color-text-primary--rgb), 0.04);
+            background-color: rgba(var(--color-bg-primary--rgb), 0.8);
+            backdrop-filter: blur(7px);
 
             .material-icons-round {
                 color: rgb(214, 0, 0);
